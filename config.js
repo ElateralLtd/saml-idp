0f25726e050a0b598a40030385fa0930a3b2a330
davesykeselateral <dave.sykes@elateral.com>
2018-05-15 11:00:29 +0100

    initial commit including brandgility specific config.js

--- config.js
+++ config.js
@@ -7,10 +7,8 @@ var profile = {
   nameIdFormat: 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
   firstName: 'Saml',
   lastName: 'Jackson',
-  displayName: 'saml jackson',
   email: 'saml.jackson@example.com',
-  mobilePhone: '+1-415-555-5141',
-  groups: 'Simple IdP Users, West Coast Users, Cloud Users'
+  role: 'USER'
 }
 
 /**
@@ -29,35 +27,17 @@ var metadata = [{
   description: 'The surname of the user',
   multiValue: false
 }, {
-  id: "displayName",
-  optional: true,
-  displayName: 'Display Name',
-  description: 'The display name of the user',
-  multiValue: false
-}, {
   id: "email",
   optional: false,
   displayName: 'E-Mail Address',
   description: 'The e-mail address of the user',
   multiValue: false
-},{
-  id: "mobilePhone",
-  optional: true,
-  displayName: 'Mobile Phone',
-  description: 'The mobile phone of the user',
-  multiValue: false
 }, {
-  id: "groups",
+  id: "role",
   optional: true,
-  displayName: 'Groups',
-  description: 'Group memberships of the user',
-  multiValue: true
-}, {
-  id: "userType",
-  optional: true,
-  displayName: 'User Type',
+  displayName: 'Role',
   description: 'The type of user',
-  options: ['Admin', 'Editor', 'Commenter']
+  options: ['ADMIN', 'AUTHOR', 'USER']
 }];
 
 module.exports = {
